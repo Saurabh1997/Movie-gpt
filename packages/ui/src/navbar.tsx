0ca8a6a667053aa81f30e8645e36d346f27b0006
sashokpatil <sashokpatil@deloitte.com>
2024-04-19 00:44:33 +0530

    generated turborepo codebase

---
+++ packages/ui/src/navbar.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+type option = {
+  displayName: string;
+  path: string;
+};
+type props = {
+  options: option[];
+};
+
+export function Navbar({ options }: props) {
+  const nav_menu = options.map((menu) => {
+    return (
+      <li key={menu?.displayName} className={``}>
+        {/* <Link to={`${menu?.path}`}>{menu?.displayName}</Link> */}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <nav
+        className={
+          "p-2 m-0 list-none overflow-hidden flex justify-between bg-slate-700"
+        }
+      >
+        {nav_menu}
+      </nav>
+    </div>
+  );
+}
